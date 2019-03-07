31756e374a2aac30b2cb417e94dfb8fcf2cd4c35
pan9 <sunzq@missfresh.cn>
2019-03-07 23:53:13 +0800

    报价接口

--- src/controllers/price.js
+++ src/controllers/price.js
@@ -1,9 +1,42 @@
 import models from '../models/index'
 export let Get =async (ctx) => {
   console.log(`access Get~`)
-  let result = await models.group.groupDB.findAll()
+  models.price.priceDB.belongsTo(models.model.modelDB, {foreignKey:'modelId', targetKey: 'id'})
+  let group = await models.group.groupDB.findAll({
+    attributes: ['id', ['name', 'modelName']]
+  })
+  let result = []
+  let fn = (elem) => {
+    return new Promise(async (resolve) => {
+      let groupId = elem.dataValues.id
+      let groupName = elem.dataValues.modelName
+      let priceObj = {}
+      let priceSingle = await models.price.priceDB.findAll({
+        include: [{
+          model: models.model.modelDB,
+          attributes: [['name', 'modelName'], 'color'],
+        }],
+        attributes: ['id', 'price', 'modelId', 'groupId', 'country'],
+        where: {groupId,status:0}
+      }) //查询价格（左连接型号表）
+      for(let i in priceSingle) {
+        for(let j in priceSingle[i].sp_model.dataValues) {
+          priceSingle[i].dataValues[j] = priceSingle[i].sp_model.dataValues[j]
+        } //遍历model 把结果循环到外层
+        delete priceSingle[i].dataValues.sp_model //遍历完成后删除model
+      }
+      priceObj = {
+        groupId,
+        groupName,
+        item: priceSingle
+      }
+      result.push(priceObj)
+      resolve()
+    })
+  }
+  await Promise.all(group.map(fn)) //等待group遍历处理完成
   ctx.body = {
     result: result,
-    succuss: true
+    status: 200
   }
 }
--- src/models/model.js
+++ src/models/model.js
@@ -5,7 +5,22 @@ export const modelDB = defineModel('sp_model', {
     groupId: Sequelize.INTEGER,
     name: Sequelize.STRING,
     color: Sequelize.STRING,
-    country: Sequelize.STRING,
+    country: {
+        type: Sequelize.INTEGER,
+        get() {
+            if(!this.getDataValue('country')) {return} //使用get()后，左连接查询时，字段必现并取到undefined
+            switch (this.getDataValue('country')) {
+                case 1:
+                    return '国行'
+                    break
+                case 2:
+                    return '港行'
+                    break
+                default:
+                    return '未知版本'
+            }
+        }
+    },
     chartsUrl: Sequelize.STRING, //趋势图链接
     originText: Sequelize.STRING //型号识别文字
 });
