314c4776c78a901fb04d734f577dc85ed9c9da94
pan9 <438063442@qq.com>
2019-04-02 02:30:49 +0800

    增加反馈

--- src/controllers/extra.js
+++ src/controllers/extra.js
@@ -1,9 +1,21 @@
 import models from '../models/index'
-import {res} from '../tool/Common'
+import { res, notNull } from '../tool/Common'
 
 export let updateTime = async (ctx) => {
- let result = await models.price.priceDB.max('updateAt',{
-   where:{status:0}
- })
- ctx.body = res(result,'success')
+  let result = await models.price.priceDB.max('updateAt', {
+    where: { status: 0 }
+  })
+  ctx.body = res(result, 'success')
+}
+
+export let wxappFeedback = async (ctx) => {
+  await models.feedback.feddbackDB()
+  let body = ctx.request.body,
+    name = notNull(name, '姓名'),
+    email = notNull(email, '邮箱'),
+    feedback = notNull(feedback, '意见'),
+    userId = ctx.state.userId,
+    result
+    result = await models.feedback.feddbackDB.create({name, email, feedback, userId})
+    ctx.body = res({ id: result.id }, '感谢您的反馈！')
 }
