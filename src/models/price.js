91913c5029201cdfc3c25739644b1482b3a103f9
pan9 <438063442@qq.com>
2019-05-17 00:51:54 +0800

    返回国家格式化

--- src/models/price.js
+++ src/models/price.js
@@ -1,9 +1,23 @@
-import {defineModel} from '../lib/sequelize'
+import { defineModel } from '../lib/sequelize'
 import Sequelize from 'sequelize'
 
 export const priceDB = defineModel('sp_price', {
-    price: Sequelize.STRING,
-    modelId: Sequelize.INTEGER,
-    groupId: Sequelize.INTEGER,
-    country: Sequelize.INTEGER,
+  price: Sequelize.STRING,
+  modelId: Sequelize.INTEGER,
+  groupId: Sequelize.INTEGER,
+  country: {
+    type: Sequelize.INTEGER,
+    get() {
+      switch (this.getDataValue('country')) {
+        case 1:
+          return '国行'
+          break
+        case 2:
+          return '港行'
+          break
+        default:
+          return '未知版本'
+      }
+    }
+  },
 });
